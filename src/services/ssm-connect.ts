33ba42245defddbeff99f3f1988d4eec85672163
brijeshroy <brijroy2502@gmail.com>
2025-12-28 01:23:36 +0530

    code fix

--- src/services/ssm-connect.ts
+++ src/services/ssm-connect.ts
@@ -7,29 +7,34 @@ let cachedSSMConfig: Record<string, Record<string, string>> | null = {};
 const ssmClient = new SSMClient({ region: process.env.AWS_REGION! });
 
 export const getSSMParam = async (cacheKey: string, ssmParamArr: string[]) => {
-  if (cachedSSMConfig.cacheKey) {
-    console.log("SSM confiog exists", JSON.stringify(cachedSSMConfig.cacheKey));
-    return cachedSSMConfig.cacheKey;
+  if (cachedSSMConfig[cacheKey]) {
+    console.log("SSM config exists", JSON.stringify(cachedSSMConfig[cacheKey]));
+    return cachedSSMConfig[cacheKey];
   }
 
   console.log("Creating New CachedSSMConfig");
 
-  const paramArray = ssmParamArr;
-  const ssmParamSet = new GetParametersCommand({
-    Names: paramArray,
-    WithDecryption: true,
-  });
+  try {
+    const ssmParamSet = new GetParametersCommand({
+      Names: ssmParamArr,
+      WithDecryption: true,
+    });
 
-  const ssmParamRes = await ssmClient.send(ssmParamSet);
-  console.log(`The Recevied SSMParam is ${JSON.stringify(ssmParamRes)}`);
+    const ssmParamRes = await ssmClient.send(ssmParamSet);
+    console.log(`The Received SSMParam is ${JSON.stringify(ssmParamRes)}`);
 
-  const params: Record<string, string> = {};
-  ssmParamRes.Parameters?.forEach((ele) => {
-    if (ele.Name && ele.Value)
-         params[ele.Name] = ele.Value;
-  });
-  cachedSSMConfig[cacheKey] = params;
-  console.log("The SSM Param object is", JSON.stringify(params));
+    const params: Record<string, string> = {};
+    ssmParamRes.Parameters?.forEach((ele) => {
+      if (ele.Name && ele.Value) {
+        params[ele.Name] = ele.Value;
+      }
+    });
+    cachedSSMConfig[cacheKey] = params;
+    console.log("The SSM Param object is", JSON.stringify(params));
 
-  return cachedSSMConfig[cacheKey];
+    return cachedSSMConfig[cacheKey];
+  } catch (error) {
+    console.error("Error fetching SSM parameters:", error);
+    throw error;
+  }
 };
