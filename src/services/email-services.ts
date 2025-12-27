751e3d52ab51d3990fedeed53fb879a2ed109f15
brijeshroy <brijroy2502@gmail.com>
2025-12-28 03:01:03 +0530

    debug

--- src/services/email-services.ts
+++ src/services/email-services.ts
@@ -1,46 +1,82 @@
 import { userCreateEmailPayload } from "../helpers/create-email-payload-helper";
 import { UserDetails } from "../interfaces/sqsBody/user-creation";
-// import { getSSMParam } from "./ssm-connect";
+// // import { getSSMParam } from "./ssm-connect";
 import  sgMail from "@sendgrid/mail"
 
 
-export const sendUserCreateEmailServices = async  (userInfo : UserDetails)=>{
+// export const sendUserCreateEmailServices = async  (userInfo : UserDetails)=>{
 
-//     const ssmParamArr = ["/user/email-api-key","/user/master-email"]
-//     let emailSSMConfig : any;
-//     try{
-//  emailSSMConfig = await  getSSMParam("email" ,ssmParamArr);
-//     }
-//     catch(err)
-//     {
-//         console.log("Error is", JSON.stringify(err))
-//         throw err;
-//     }
-    // const senderEmailId = emailSSMConfig["/user/email-api-key"]!;
+// //     const ssmParamArr = ["/user/email-api-key","/user/master-email"]
+// //     let emailSSMConfig : any;
+// //     try{
+// //  emailSSMConfig = await  getSSMParam("email" ,ssmParamArr);
+// //     }
+// //     catch(err)
+// //     {
+// //         console.log("Error is", JSON.stringify(err))
+// //         throw err;
+// //     }
+//     // const senderEmailId = emailSSMConfig["/user/email-api-key"]!;
 
-    const emailPayload = userCreateEmailPayload(userInfo,process.env.API_EMAIL!)
-    console.log("Sending message")
+//     const emailPayload = userCreateEmailPayload(userInfo,process.env.API_EMAIL!)
+//     console.log("Sending message")
 
-    sgMail.setApiKey(process.env.API_KEY!)
+//     sgMail.setApiKey(process.env.API_KEY!)
   
-    try{
-    await sgMail.send(emailPayload)
-    console.log("Email Successfully sent")
-    }
-    catch(err:any)
-    {
-         console.error("SendGrid error message:", err?.message);
+//     try{
+//            console.log("Sending message")
+//     await sgMail.send(emailPayload)
+//     console.log("Email Successfully sent")
+//     }
+//     catch(err:any)
+//     {
+//          console.error("SendGrid error message:", err?.message);
 
-  if (err?.response?.body) {
-    console.error(
-      "SendGrid response body:",
-      JSON.stringify(err.response.body, null, 2)
-    );
+//   if (err?.response?.body) {
+//     console.error(
+//       "SendGrid response body:",
+//       JSON.stringify(err.response.body, null, 2)
+//     );
+//   }
+
+//          throw err;
+//     }
+
+
+
+// }
+
+
+
+
+sgMail.setApiKey(process.env.API_KEY!);
+
+export const sendUserCreateEmailServices = async (userInfo: any) => {
+  const emailPayload = userCreateEmailPayload(
+    userInfo,
+    process.env.API_EMAIL!
+  );
+
+  console.log("EMAIL PAYLOAD >>>", JSON.stringify(emailPayload, null, 2));
+
+  try {
+    const response = await sgMail.send(emailPayload);
+    console.log("SENDGRID RESPONSE >>>", JSON.stringify(response, null, 2));
+  } catch (err: any) {
+    console.error("SENDGRID ERROR MESSAGE >>>", err?.message);
+    console.error("SENDGRID ERROR STACK >>>", err?.stack);
+
+    if (err?.response) {
+      console.error(
+        "SENDGRID RESPONSE STATUS >>>",
+        err.response.statusCode
+      );
+      console.error(
+        "SENDGRID RESPONSE BODY >>>",
+        JSON.stringify(err.response.body, null, 2)
+      );
+    }
+
+    throw err;
   }
-
-         throw err;
-    }
-
-
-
-}
+};
